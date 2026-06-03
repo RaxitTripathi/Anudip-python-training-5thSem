f9a52e1eb07e7ca367782392f43403986a0a3842
RaxitTripathi <raxit08tripathi@gmail.com>
2026-06-03 12:17:35 +0530

    Program to CALCULATE AREA AND PARAMETER OF TRIANGLE

--- ClassWork/triangleOperation.py
+++ ClassWork/triangleOperation.py
@@ -5,6 +5,11 @@ side1=float(input("Enter first side (in cm)="))
 side2=float(input("Enter first side (in cm)="))
 side3=float(input("Enter first side (in cm)="))
 #-------------------------------------------------
+print("-----------------------")
+print("First Side :",side1,"cm")
+print("Second Side :",side2,"cm")
+print("Third Side :",side3,"cm")
+#-------------------------------------------------
 parameter=side1+side2+side3
 #Need to calculate Area
 s=(side1+side2+side3)/2
