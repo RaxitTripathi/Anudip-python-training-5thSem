09cc60da9a2104e45aaebc6b061417da9b34ae29
RaxitTripathi <raxit08tripathi@gmail.com>
2026-06-03 15:11:16 +0530

    Wap validation changes

--- LabWork/SelectionStatement/rectangleOperation.py
+++ LabWork/SelectionStatement/rectangleOperation.py
@@ -1,17 +1,17 @@
 #Wap to calculate area and parameter of a rectangle 
 
-print("----- Rectangle -----")
+
 #----------------Length-----------------------
 length = float(input("Enter length (cm): "))
+if(length <=0 ):
+    exit("Length must be > 0")
 #----------------Breadth-----------------------
 breadth = float(input("Enter breadth (cm): "))
-
+if(breadth <=0 ):
+    exit("Breadth must be > 0")
 #------------------Inputs------------------------
 print("Length=",length)
 print("Breadth=",breadth)
-#----------------Validation---------------------
-if length <= 0 or breadth <= 0:
-    exit("Length and breadth must be > 0")
 #----------Area and parimeter--------------
 area = length * breadth
 perimeter = 2 * (length + breadth)
--- LabWork/SelectionStatement/simpleInterest.py
+++ LabWork/SelectionStatement/simpleInterest.py
@@ -1,14 +1,21 @@
 #WAP to calculate Simple Interest 
 print("-----------Simple Interest--------------")
+
 principle=float(input("Enter principle amount: "))
+if(principle<=0):
+    exit("Invalid input... Exited")
+
 rate=float(input("Enter rate(%): "))
+if(rate<0):
+    exit("Invalid input... Exited")
+
 time=int(input("Enter Time(in years)"))
+if(time<0):
+    exit("Invalid input... Exited")
 #----------------Print-----------------------
 print("Principle=",principle)
 print("Rate=",rate)
 print("Time=",time)
-#------------Validation----------------------
-if(principle<=0 or rate<0 or time<0):
-    exit("Invalid input... Exited")
+
 
 print("Simple Interest =₹",(principle*rate*time)/100)
