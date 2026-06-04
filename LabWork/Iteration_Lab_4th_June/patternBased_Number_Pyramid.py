4859c605f3773ee4515d92c092928bb9fdafa870
RaxitTripathi <raxit08tripathi@gmail.com>
2026-06-04 15:41:52 +0530

    Pattern Printing

---
+++ LabWork/Iteration_Lab_4th_June/patternBased_Number_Pyramid.py
@@ -0,0 +1,31 @@
+# Problem Statement: 
+# Accept the number of rows and print the following pattern: 
+# For Input: 
+# 5 
+# Output: 
+# 1 
+# 12 
+# 123 
+# 1234 
+# 12345 
+# Challenge: 
+# Print the reverse pattern as well.
+
+
+pattern_row=int(input("Enter the no of row for pattern:"))
+if(pattern_row<0):
+    exit("Negative number not allowed:")
+
+for i in range(1,pattern_row+1):
+    for j in range(1,i+1):
+        print(j,end="")
+    print()    
+
+# Print the reverse pattern as well.
+
+print()
+print("-----------Its reverse-----------")
+for i in range(pattern_row,0,-1):
+    for j in range(1,i+1):
+        print(j,end="")
+    print()    
