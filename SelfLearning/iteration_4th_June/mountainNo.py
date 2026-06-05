783acf2556aae0b4a5391727db4b5b18bade1fce
RaxitTripathi <raxit08tripathi@gmail.com>
2026-06-05 11:14:29 +0530

    mountain number

---
+++ SelfLearning/iteration_4th_June/mountainNo.py
@@ -0,0 +1,9 @@
+# A mountain number must follow this pattern:
+
+# Digits strictly increase first
+# Then strictly decrease
+# Only one peak
+# No repetition at the turning point
+
+num = int(input("Enter a number: "))
+
