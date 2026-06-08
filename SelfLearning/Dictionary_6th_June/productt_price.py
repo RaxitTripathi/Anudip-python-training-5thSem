3dc2e635a03d1a13544a4a9f193aa3f8effb2cb3
RaxitTripathi <raxit08tripathi@gmail.com>
2026-06-08 05:39:13 +0530

    Product Price Analysis

---
+++ SelfLearning/Dictionary_6th_June/productt_price.py
@@ -0,0 +1,69 @@
+""" 6. Product Price Analysis 
+Sample Data 
+prices = { 
+    "Laptop": 55000, 
+    "Mouse": 800, 
+    "Keyboard": 1800, 
+    "Monitor": 12000, 
+    "Printer": 9000, 
+    "Tablet": 28000, 
+    "Speaker": 3500, 
+    "Webcam": 2500, 
+    "Headphones": 4200, 
+    "Router": 3200 
+} 
+Tasks 
+• Display products costing more than ₹5000.  
+• Count products costing less than ₹3000.  
+• Find the most expensive product.  
+• Create a list of products priced between ₹2000 and ₹10000.  
+• Calculate the total value of all products.   """
+
+prices = {
+    "Laptop": 55000,
+    "Mouse": 800,
+    "Keyboard": 1800,
+    "Monitor": 12000,
+    "Printer": 9000,
+    "Tablet": 28000,
+    "Speaker": 3500,
+    "Webcam": 2500,
+    "Headphones": 4200,
+    "Router": 3200
+}
+
+# Display products costing more than ₹5000
+print("Products costing more than ₹5000:")
+for product, price in prices.items():
+    if price > 5000:
+        print(product, ":", price)
+
+# Count products costing less than ₹3000
+count = 0
+for price in prices.values():
+    if price < 3000:
+        count += 1
+
+print("\nNumber of products costing less than ₹3000:", count)
+
+# Find the most expensive product
+expensive_product = max(prices, key=prices.get)
+print("\nMost expensive product:", expensive_product)
+print("Price:", prices[expensive_product])
+
+# Create a list of products priced between ₹2000 and ₹10000
+mid_range_products = []
+for product, price in prices.items():
+    if 2000 <= price <= 10000:
+        mid_range_products.append(product)
+
+print("\nProducts priced between ₹2000 and ₹10000:")
+print(mid_range_products)
+
+# Calculate the total value of all products
+total_value = 0
+
+for price in prices.values():
+    total_value += price
+
+print("\nTotal value of all products:", total_value)
