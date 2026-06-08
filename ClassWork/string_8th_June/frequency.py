9d57f60add774564d2c413c7c4609b00e2dbf4df
RaxitTripathi <raxit08tripathi@gmail.com>
2026-06-08 15:39:22 +0530

    frequency of vowel present

---
+++ ClassWork/string_8th_June/frequency.py
@@ -0,0 +1,14 @@
+# wap to input a sentence and display the frequency of vowel present ignore the case
+
+
+user_input=input("Enter sentence:").lower()
+vowel_dict={}
+
+for letter in user_input:
+    if letter in "aeiou":
+        if letter in vowel_dict:
+            vowel_dict[letter] +=1
+        else:
+            vowel_dict[letter]=1    
+        
+print("Total vowel in given sentence is",vowel_dict)        
