e33faa11254ecae13f195b6f51432dda906b4503
RaxitTripathi <raxit08tripathi@gmail.com>
2026-06-09 11:21:45 +0530

    4. Vehicle Number Plate Verification

--- LabWork/string_8th_June/vehicle_number_plate.py
+++ LabWork/string_8th_June/vehicle_number_plate.py
@@ -19,7 +19,7 @@ o Last 4 must be digits.
 plate = "MH12AB4589"
 
 if len(plate) == 0:
-    exit("Vechile plate no not valid")
+    exit("Vechile plate not valid")
 
 # 1. Extract state code
 state_code = plate[0:2]
